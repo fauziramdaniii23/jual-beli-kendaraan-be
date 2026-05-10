f0f6caaf55f2dff37cb2e67a020811f27d4b2dba
fauziramdani <fauziramdani234@gmail.com>
2026-05-10 21:58:43 +0700

    init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function test()
+    {
+        $user = auth()->user();
+        return response()->json([
+            'message' => 'Hello, World!',
+            'user' => $user,
+        ]);
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
+Route::get('/test', [Controller::class, 'test'])->middleware('auth:sanctum');
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello, World!']);
+});
